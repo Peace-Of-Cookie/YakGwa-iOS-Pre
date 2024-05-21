2037c27cc9449ba7127e5fe7bdc0729d8a4d93e6
Ekko0701 <djekko0701@gmail.com>
2024-05-21 16:58:20 +0900

    Refactor: Convert Storyboard to Codebase, Add CoreKit, DesignSystem, and Network Module
    - Replaced Storyboard with programmatic UI implementation using UIKit
    - Integrated CoreKit module for core functionalities
    - Added DesignSystem module for consistent styling and UI components
    - Implemented Network module for API interactions and data fetching

---
+++ YakGwa/Core/CoreKit/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CoreKit",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "CoreKit",
+            targets: ["CoreKit"]),
+    ],
+    dependencies: [
+        .package(path: "./DesignSystem"),
+    ],
+    targets: [
+        .target(
+            name: "CoreKit",
+            dependencies: [
+                .product(name: "DesignSystem", package: "DesignSystem"),
+            ]
+        ),
+        .testTarget(
+            name: "CoreKitTests",
+            dependencies: ["CoreKit"]),
+    ]
+)
--- YakGwa/ViewController.swift
+++ YakGwa/ViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
+import DesignSystem
+
 class ViewController: UIViewController {
 
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "YakGwa"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBlue
+        setUI()
+    }
+    
+    func setUI() {
+        self.view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.centerX.equalToSuperview()
+        }
     }
 }
 
