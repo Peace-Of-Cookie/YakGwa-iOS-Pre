46bfb8262dc825d7b99ab319d027aff501084065
DongJoo Kim <djekko0701@gmail.com>
2024-06-04 16:45:38 +0900

    feat: MakeYakgwaReactor State 정의

--- YakGwa/Scene/MakeYakgwaScene/Sources/MakeYakgwaScene/Presentation/MakeYakgwaReactor.swift
+++ YakGwa/Scene/MakeYakgwaScene/Sources/MakeYakgwaScene/Presentation/MakeYakgwaReactor.swift
@@ -5,16 +5,43 @@
 //  Created by Ekko on 6/3/24.
 //
 
+import Foundation
+
 import ReactorKit
 
 public final class MakeYakgwaReactor: Reactor {
     public enum Action {
+        case confirmButtonTapped
     }
     
     public enum Mutation {
+        // case setYakgwaInfo()
     }
     
     public struct State {
+        /// 약속 타이틀
+        var yakgwaTitle: String?
+        /// 약속 설명
+        var yakgwaDescription: String?
+        /// 약속 테마
+        var yakgwaTheme: String?
+        /// 이미 장소가 결졍된 여부
+        var alreadySelectedLocation: Bool = false
+        /// 약속 장소
+        var yakgwaLocation: [String] = []
+        /// 이미 시간이 정해진 여부
+        var alreadySelectedDate: Bool = false
+        /// 약속 시작 날짜
+        var yakgwaStartDate: Date?
+        /// 약속 종료 날짜
+        var yakgwaEndDate: Date?
+        /// 약속 시작 시간
+        var yakgwaStartTime: Date?
+        /// 약속 종료 시간
+        var yakgwaEndTime: Date?
+        /// 초대 마감 시간
+        var expiredDate: Date?
+        
     }
     
     public var initialState: State
