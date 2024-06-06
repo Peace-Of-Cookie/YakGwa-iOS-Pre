27c3a485a41ec789e7a0ffafec5c2e840ad86279
DongJoo Kim <djekko0701@gmail.com>
2024-06-07 05:04:06 +0900

    ui: 장소 투표 화면 ui 컴포넌트 추가

--- YakGwa/Scene/YakgwaDetailScene/Sources/YakgwaDetailScene/Presentation/Place/PlaceVoteViewController.swift
+++ YakGwa/Scene/YakgwaDetailScene/Sources/YakgwaDetailScene/Presentation/Place/PlaceVoteViewController.swift
@@ -1,6 +1,6 @@
 //
 //  PlaceVoteViewController.swift
-//  
+//
 //
 //  Created by Ekko on 6/7/24.
 //
@@ -19,9 +19,27 @@ public final class PlaceVoteViewController: UIViewController, View {
     public var coordinator: PlaceVoteCoordinator?
     
     // MARK: - UI Components
+    private lazy var placeTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(PlaceCell.self, forCellReuseIdentifier: PlaceCell.identifier)
+        return tableView
+    }()
+    
+    private lazy var bottomButtonContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .neutralWhite
+        view.layer.applySketchShadow(color: .neutral600, alpha: 0.2, x: 0, y: -1, blur: 20, spread: 0)
+        return view
+    }()
+
+    private lazy var confirmButton: YakGwaButton = {
+        let button = YakGwaButton()
+        button.title = "투표 완료"
+        return button
+    }()
     
     // MARK: - Initializers
-    init(reactor: PlaceVoteViewReactor) {
+    public init(reactor: PlaceVoteViewReactor) {
         super.init(nibName: nil, bundle: nil)
         self.reactor = reactor
     }
@@ -32,12 +50,53 @@ public final class PlaceVoteViewController: UIViewController, View {
     // MARK: - Life cycles
     public override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemRed
+        self.view.backgroundColor = .neutral200
+        self.setUI()
     }
     // MARK: - Layout
+    private func setUI() {
+        self.view.addSubview(placeTableView)
+        placeTableView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(100)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        
+        self.view.addSubview(bottomButtonContainer)
+        bottomButtonContainer.snp.makeConstraints {
+            $0.height.equalTo(92)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        bottomButtonContainer.addSubview(confirmButton)
+        confirmButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(8)
+            $0.leading.equalToSuperview().offset(16)
+            $0.centerX.equalToSuperview()
+        }
+    }
     
     // MARK: - Binding
     public func bind(reactor: PlaceVoteViewReactor) {
+        // Action
+        self.rx.viewWillAppear
+            .map { _ in Reactor.Action.viewWillAppeared }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
+        // State
+        reactor.state
+            .map { $0.places }
+            .distinctUntilChanged()
+            .subscribe(onNext: { [weak self] places in
+                print("테스트 \(places)")
+            }).disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.places ?? [] }
+            .distinctUntilChanged()
+            .bind(to: placeTableView.rx.items(cellIdentifier: PlaceCell.identifier, cellType: PlaceCell.self)) { index, place, cell in
+                cell.configure(place: place)
+            }.disposed(by: disposeBag)
     }
 }
--- YakGwa/Scene/YakgwaDetailScene/Sources/YakgwaDetailScene/Presentation/Place/PlaceVoteViewReactor.swift
+++ YakGwa/Scene/YakgwaDetailScene/Sources/YakgwaDetailScene/Presentation/Place/PlaceVoteViewReactor.swift
@@ -14,23 +14,47 @@ public final class PlaceVoteViewReactor: Reactor {
     let disposeBag: DisposeBag = DisposeBag()
     
     public enum Action {
-        
+        case viewWillAppeared
     }
     
     public enum Mutation {
-        
+        case setPlaces([MeetVoteInfo.RecommendPlace])
     }
     
     public struct State {
-        
+        var meetId: Int
+        var places: [MeetVoteInfo.RecommendPlace]?
     }
     
-    public var initialState: State = State()
+    public var initialState: State
     
-    init(
-        meetId: Int,
+    public init(
+        meetId: Int = 40,
         fetchMeetVoteInfoUseCase: FetchMeetVoteInfoUseCaseProtocol
     ) {
+        self.initialState = State(meetId: meetId)
         self.fetchMeetVoteInfoUseCase = fetchMeetVoteInfoUseCase
+    }
+    
+    public func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewWillAppeared:
+            return fetchMeetVoteInfoUseCase
+                .execute(meetId: currentState.meetId)
+                .map { result in
+                    result.recommendPlaces ?? []
+                }.asObservable()
+                .map { Mutation.setPlaces($0)}
+        }
+    }
+    
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setPlaces(let places):
+            newState.places = places
+        }
+        
+        return newState
     }
 }
