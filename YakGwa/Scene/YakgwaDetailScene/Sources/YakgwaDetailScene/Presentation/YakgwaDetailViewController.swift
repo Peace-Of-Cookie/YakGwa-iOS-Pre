1bd1f10abbd3d2ba3ebb7b946471be833b7d4f8e
DongJoo Kim <djekko0701@gmail.com>
2024-06-06 12:44:22 +0900

    chore: Update minimum iPhone deployment target to iOS 15.0

--- YakGwa/Scene/YakgwaDetailScene/Sources/YakgwaDetailScene/Presentation/YakgwaDetailViewController.swift
+++ YakGwa/Scene/YakgwaDetailScene/Sources/YakgwaDetailScene/Presentation/YakgwaDetailViewController.swift
@@ -7,9 +7,36 @@
 
 import UIKit
 
-final class YakgwaDetailViewController: UIViewController {
-    override func viewDidLoad() {
+import CoreKit
+import Util
+
+import ReactorKit
+import RxCocoa
+
+public final class YakgwaDetailViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    // MARK: - Initializers
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life cycles
+    public override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemRed
     }
+    
+    // MARK: - Layout
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Binding
 }
